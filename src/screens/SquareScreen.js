f29cec8aaf0cf5989f64feba430d458c40e63ece
Yichuan Wang <w369371@gmail.com>
2019-12-02 11:03:30 +1000

    add bounds to colour increaser and decreaser

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,18 +2,37 @@ import React, {useState} from 'react';
 import ColourCounter from '../components/ColourCounter'
 import {View , Text, StyleSheet, Image, Button} from 'react-native'
 
-const COLOUR_INCREMENT = 10
+const COLOUR_INCREMENT = 20
 
 const SquareScreen = () => {
     const [red, setRed] = useState(0)
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
+
+    const setColour = (colour, increment) => {
+         if(colour + increment < 0 || colour + increment > 255 ){
+             return
+         }
+         switch(colour){
+             case "red":
+                setRed(red + increment)
+                return
+             case "green":
+                setGreen(green + increment)
+                return
+             case "blue":
+               setBlue(blue + increment)
+               return
+             default:
+               return
+         }
+    }
     
     return (
     <View>     
-      <ColourCounter onIncrease={() => setRed(red + COLOUR_INCREMENT)} onDecrease={() => setRed(red - COLOUR_INCREMENT)} colourName="Red"/> 
-      <ColourCounter onIncrease={() => setGreen(green + COLOUR_INCREMENT)} onDecrease={() => setGreen(green - COLOUR_INCREMENT)} colourName="Green"/>
-      <ColourCounter onIncrease={() => setBlue(blue + COLOUR_INCREMENT)} onDecrease={() => setBlue(blue - COLOUR_INCREMENT)} colourName="Blue"/>
+      <ColourCounter onIncrease={() => setColour("red", COLOUR_INCREMENT)} onDecrease={() => setColour("red", -1*COLOUR_INCREMENT)} colourName="Red"/> 
+      <ColourCounter onIncrease={() => setColour("green", COLOUR_INCREMENT)} onDecrease={() => setColour("green", -1* COLOUR_INCREMENT)} colourName="Green"/>
+      <ColourCounter onIncrease={() => setColour("blue", COLOUR_INCREMENT)} onDecrease={() => setColour("blue", -1*COLOUR_INCREMENT)} colourName="Blue"/>
       <View style = {{height: 150, width:150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
     </View>
     
